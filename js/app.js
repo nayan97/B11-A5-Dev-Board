bd0fc41c026aa2a04aa9f50b3b4b2835c174a23d
nayan97 <nayanislam085@gmail.com>
2025-03-02 23:34:20 +0600

    link

--- js/app.js
+++ js/app.js
@@ -103,7 +103,7 @@ function bgColor() {
 document.getElementById("blog")
 .addEventListener("click", function(event){
     event.preventDefault();
-    window.location.href = "/blog.html";
+    window.location.href = "blog.html";
 
 });
 
